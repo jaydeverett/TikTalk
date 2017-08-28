1e3b29513e6f7ac421eb5238c84ab5714c92dae9
Jay Deverett <jayjdeverett@gmail.com>
2017-08-28 18:28:58 -0400

    add manual break to redd loop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,16 +13,21 @@ class ArticlesController < ApplicationController
   def index
         session = Redd.it(
       client_id:  'tR3gd1Ylo9swdg',
-      secret:     '1WNzeFaOGVelOB2hl60XBtyY3AY',
+      secret:     '1WNzeFaOGVelOB2hl60XBtyY3AY'
     )
 
-    @articles = []
+    @articles=[]
 
-    session.subreddit('showerthoughts').post_stream(limit:5) do |post|
-      byebug
-      @articles << post.title
+    i=0
+    session.subreddit('worldnews').post_stream(limit:5) do |post|
+
+        @articles << post.title
+        i +=1
+        if i >= 5
+          break
+        end
+
     end
-    byebug
   end
 
   def show
