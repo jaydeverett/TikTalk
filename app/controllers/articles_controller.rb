1719979a64908f87d948c63c3632cc86e345cb5d
Jay Deverett <jayjdeverett@gmail.com>
2017-08-28 17:49:51 -0400

    add first pass at reddit api using redd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  require 'redd'
+
   def new
 
   end
@@ -9,7 +11,18 @@ class ArticlesController < ApplicationController
   end
 
   def index
+        session = Redd.it(
+      client_id:  'tR3gd1Ylo9swdg',
+      secret:     '1WNzeFaOGVelOB2hl60XBtyY3AY',
+    )
 
+    @articles = []
+
+    session.subreddit('showerthoughts').post_stream(limit:5) do |post|
+      byebug
+      @articles << post.title
+    end
+    byebug
   end
 
   def show
