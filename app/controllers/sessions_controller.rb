717ec87cf743236b9998bb8f39ef84b65ec317ff
Jay Deverett <jayjdeverett@gmail.com>
2017-08-28 14:41:39 -0400

    add user authentication/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to articles_path, notice: "Logged in!"
+    else
+      render "new"
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to articles_path, notice: "Logged out!"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'Account successfully created!'
+      redirect_to articles_path
+    else
+      flash.now[:error] = 'Sorry try again!'
+      render :new
+    end
   end
-  
 
-end
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+ end
