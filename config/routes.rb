9a87eae0cfcd6b426214facd2ddd1bd7aaf5eab7
shaanray <shaanrayoffice@gmail.com>
2017-09-04 17:18:43 -0400

    add CRUD actions/partials to user form/showpage. Make minor visual front-end edits

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'articles/:id/comments'=> 'comments#show', as: :allcomments
-  resources :users, only: [:new, :create, :show]
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :articles do
     resources :comments do
