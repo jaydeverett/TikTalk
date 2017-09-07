e6697a566a1a872ca8fb08e7e75fb5c92118b1be
Jay Deverett <jayjdeverett@gmail.com>
2017-09-07 17:27:55 -0400

    fixed migration for version 0

--- db/migrate/20170906192651_change_default_total_wins_to_zero.rb
+++ db/migrate/20170906192651_change_default_total_wins_to_zero.rb
@@ -1,8 +1,10 @@
 class ChangeDefaultTotalWinsToZero < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_table :users do |t|
       t.remove :total_wins
       t.integer :total_wins, default: 0, null: false
+    end
+    def down
     end
   end
 end
