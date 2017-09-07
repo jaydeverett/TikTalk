5573c60017c433bc9de9294d4a3500e132c90549
Jay Deverett <jayjdeverett@gmail.com>
2017-09-07 15:52:28 -0400

    Adding both background jobs

--- app/jobs/work_job.rb
+++ app/jobs/work_job.rb
@@ -4,7 +4,7 @@ class WorkJob < ApplicationJob
 
   def perform(*args)
     puts "Staring Tasks"
-    system("cd #{Rails.root} && RAILS_ENV=#{Rails.env} bundle exec rake livefeed:reddit >> log/delayed_rake.log")
-
+    system("nohup cd #{Rails.root} && RAILS_ENV=#{Rails.env} bundle exec rake livefeed:reddit >> log/delayed_rake.log 2>&1 &")
+    system("nohup cd #{Rails.root} && RAILS_ENV=#{Rails.env} bundle exec rake expire:article >> log/delayed_rake.log 2>&1 &")
   end
 end
---
+++ test/jobs/work2_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Work2JobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
